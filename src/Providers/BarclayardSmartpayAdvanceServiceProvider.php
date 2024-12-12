d5cc953f461c2dcec1a8b397ee83f9566d11b155
Asif Hussain <azif.hussain@gmail.com>
2024-12-12 17:36:23 +0000

    first commit - created basic package scaffolding

---
+++ src/Providers/BarclayardSmartpayAdvanceServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cws\BarclaycardSmartpayAdvance\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BarclaycardSmartpayAdvanceServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $packagePath = realpath(__DIR__.'/../../');
+
+        // Publish/Merge Config
+        $this->mergeConfigFrom("{$packagePath}/config/barclaycard-smartpay-advance.php", 'barclaycard-smartpay-advance');
+        $this->publishes(["{$packagePath}/config/" => config_path()], ['config']);
+    }
+}
